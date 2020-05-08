d16909a2bb21c19e7323c09a1254542a7d48b157
calvincodes <arpitjain1821@gmail.com>
2020-05-08 01:17:40 -0700

    Adding SearchIssue model.

--- src/main/java/com/github/calvincodes/models/SearchIssue.java
+++ src/main/java/com/github/calvincodes/models/SearchIssue.java
@@ -1,7 +1,9 @@
-package com.github.calvincodes;
+package com.github.calvincodes.models;
 
 import com.google.gson.annotations.SerializedName;
-import com.sun.xml.internal.ws.developer.Serialization;
+
+import java.util.Date;
+import java.util.List;
 
 public class SearchIssue {
 
@@ -35,5 +37,267 @@ public class SearchIssue {
     @SerializedName("title")
     private String title;
 
+    @SerializedName("user")
+    private User user;
 
+    @SerializedName("labels")
+    private List<Label> labels;
+
+    @SerializedName("state")
+    private String state;
+
+    @SerializedName("locked")
+    private Boolean locked;
+
+    @SerializedName("assignee")
+    private User assignee;
+
+    @SerializedName("assignees")
+    private List<User> assignees;
+
+    @SerializedName("milestone")
+    private Milestone milestone;
+
+    @SerializedName("comments")
+    private Long comments;
+
+    @SerializedName("created_at")
+    private Date createdAt;
+
+    @SerializedName("updated_at")
+    private Date updatedAt;
+
+    @SerializedName("closed_at")
+    private Date closedAt;
+
+    @SerializedName("author_association")
+    private String authorAssociation;
+
+    @SerializedName("body")
+    private String body;
+
+    @SerializedName("score")
+    private Double score;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getRepositoryUrl() {
+        return repositoryUrl;
+    }
+
+    public void setRepositoryUrl(String repositoryUrl) {
+        this.repositoryUrl = repositoryUrl;
+    }
+
+    public String getLabelsUrl() {
+        return labelsUrl;
+    }
+
+    public void setLabelsUrl(String labelsUrl) {
+        this.labelsUrl = labelsUrl;
+    }
+
+    public String getCommentsUrl() {
+        return commentsUrl;
+    }
+
+    public void setCommentsUrl(String commentsUrl) {
+        this.commentsUrl = commentsUrl;
+    }
+
+    public String getEventsUrl() {
+        return eventsUrl;
+    }
+
+    public void setEventsUrl(String eventsUrl) {
+        this.eventsUrl = eventsUrl;
+    }
+
+    public String getHtmlUrl() {
+        return htmlUrl;
+    }
+
+    public void setHtmlUrl(String htmlUrl) {
+        this.htmlUrl = htmlUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNodeId() {
+        return nodeId;
+    }
+
+    public void setNodeId(String nodeId) {
+        this.nodeId = nodeId;
+    }
+
+    public Long getNumber() {
+        return number;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Label> getLabels() {
+        return labels;
+    }
+
+    public void setLabels(List<Label> labels) {
+        this.labels = labels;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Boolean getLocked() {
+        return locked;
+    }
+
+    public void setLocked(Boolean locked) {
+        this.locked = locked;
+    }
+
+    public User getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(User assignee) {
+        this.assignee = assignee;
+    }
+
+    public List<User> getAssignees() {
+        return assignees;
+    }
+
+    public void setAssignees(List<User> assignees) {
+        this.assignees = assignees;
+    }
+
+    public Milestone getMilestone() {
+        return milestone;
+    }
+
+    public void setMilestone(Milestone milestone) {
+        this.milestone = milestone;
+    }
+
+    public Long getComments() {
+        return comments;
+    }
+
+    public void setComments(Long comments) {
+        this.comments = comments;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Date getClosedAt() {
+        return closedAt;
+    }
+
+    public void setClosedAt(Date closedAt) {
+        this.closedAt = closedAt;
+    }
+
+    public String getAuthorAssociation() {
+        return authorAssociation;
+    }
+
+    public void setAuthorAssociation(String authorAssociation) {
+        this.authorAssociation = authorAssociation;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "SearchIssue{" +
+                "url='" + url + '\'' +
+                ", repositoryUrl='" + repositoryUrl + '\'' +
+                ", labelsUrl='" + labelsUrl + '\'' +
+                ", commentsUrl='" + commentsUrl + '\'' +
+                ", eventsUrl='" + eventsUrl + '\'' +
+                ", htmlUrl='" + htmlUrl + '\'' +
+                ", id='" + id + '\'' +
+                ", nodeId='" + nodeId + '\'' +
+                ", number=" + number +
+                ", title='" + title + '\'' +
+                ", user=" + user +
+                ", labels=" + labels +
+                ", state='" + state + '\'' +
+                ", locked=" + locked +
+                ", assignee=" + assignee +
+                ", assignees=" + assignees +
+                ", milestone=" + milestone +
+                ", comments=" + comments +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                ", closedAt=" + closedAt +
+                ", authorAssociation='" + authorAssociation + '\'' +
+                ", body='" + body + '\'' +
+                ", score=" + score +
+                '}';
+    }
 }
