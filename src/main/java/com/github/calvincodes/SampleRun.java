f62553a233f9f3da05869faba21536c4a3c6bc67
calvincodes <arpitjain1821@gmail.com>
2020-05-09 11:36:02 -0700

    Adding refactored SampleRun.

--- src/main/java/com/github/calvincodes/SampleRun.java
+++ src/main/java/com/github/calvincodes/SampleRun.java
@@ -1,35 +1,20 @@
 package com.github.calvincodes;
 
+import com.github.calvincodes.client.GitHubIssuesClient;
+import com.github.calvincodes.models.SearchIssueRequest;
 import com.github.calvincodes.models.SearchIssueResponse;
-import com.google.gson.Gson;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.net.URISyntaxException;
 
 public class SampleRun {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, URISyntaxException {
 
-        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-        //We just change HttpPost > HttpDelete
-        HttpGet getRequest = new HttpGet("https://api.github.com/search/issues?q=state%3Aopen+label%3A%22good-first-issue%22&type=Issues");
-        getRequest.addHeader("accept", "application/json");
-
-        HttpResponse response = httpClient.execute(getRequest);
-        if (response.getStatusLine().getStatusCode() != 200) {
-            throw new RuntimeException(
-                    "Failed : HTTP error code : " + response.getStatusLine().getStatusCode());
-        }
-        String result = IOUtils.toString(response.getEntity().getContent(), StandardCharsets.UTF_8);
-        response.getEntity().getContent().close();
-
-        Gson gson = new Gson();
-        SearchIssueResponse searchIssueResponse = gson.fromJson(result, SearchIssueResponse.class);
-
-        httpClient.close();
+        GitHubIssuesClient client = new GitHubIssuesClient();
+        SearchIssueRequest searchIssueRequest = new SearchIssueRequest();
+        searchIssueRequest.setState("open"); // TODO: Create enum for type. Label: Good first issue.
+        searchIssueRequest.setLabel("good-first-issue");
+        SearchIssueResponse response = client.search(searchIssueRequest);
+        System.out.println(response);
     }
 }
