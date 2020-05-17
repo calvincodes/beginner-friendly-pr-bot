3369eb603fa013a2b66bf4b86d09f127a262398a
calvincodes <arpitjain1821@gmail.com>
2020-05-17 12:25:34 -0700

    Setting up GCP cron.

---
+++ gcp-ce-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt-get update
+
+# Install Git
+sudo apt install git
+
+# Install Java JRE and JDK
+sudo apt-get install default-jre
+sudo apt-get install default-jdk
+
+# Install Maven
+sudo apt install maven
